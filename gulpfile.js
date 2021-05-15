d5529528dcfaedaa6e945758c045c525f4fafb3a
Allex24 <inta.dn@yandex.ua>
2021-05-15 19:38:11 +0300

    Выполняет автоматическую сборку личного проекта!

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@ const sync = require("browser-sync").create();
 // Styles
 
 const styles = () => {
-  return gulp.src("source/sass/style.sass")
+  return gulp.src("source/sass/style.scss")
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(sass())
@@ -25,7 +25,7 @@ const styles = () => {
       autoprefixer(),
       csso()
     ]))
-    .pipe(rename("style.min.css"))
+    .pipe(rename("style.css"))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
@@ -40,18 +40,6 @@ const html = () => {
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest("build"));
 }
-
-// Scripts
-
-const scripts = () => {
-  return gulp.src("source/js/script.js")
-    .pipe(terser())
-    .pipe(rename("script.min.js"))
-    .pipe(gulp.dest("build/js"))
-    .pipe(sync.stream());
-}
-
-exports.scripts = scripts;
 
 // Images
 
@@ -147,7 +135,6 @@ const reload = (done) => {
 
 const watcher = () => {
   gulp.watch("source/sass/**/*.sass", gulp.series(styles));
-  gulp.watch("source/js/script.js", gulp.series(scripts));
   gulp.watch("source/*.html", gulp.series(html, reload));
 }
 
@@ -160,7 +147,6 @@ const build = gulp.series(
   gulp.parallel(
     styles,
     html,
-    scripts,
     sprite,
     createWebp
   ),
@@ -178,7 +164,6 @@ exports.default = gulp.series(
   gulp.parallel(
     styles,
     html,
-    scripts,
     sprite,
     createWebp
   ),
